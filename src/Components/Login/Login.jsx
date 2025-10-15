a739166039a3346e8f0b3c214295aff97ad7f985
tahajid36 <brucextahajid@gmail.com>
2025-10-16 00:32:52 +0600

    basic setup

---
+++ src/Components/Login/Login.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Login = () => {
+    return (
+        <div>
+            <h2>
+                login page
+            </h2>
+        </div>
+    );
+};
+
+export default Login;
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,35 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import { createBrowserRouter } from "react-router";
+import { RouterProvider } from "react-router/dom";
+import Root from "./Components/Layout/Root.jsx";
+import Home from "./Components/Home/Home.jsx";
+import Login from "./Components/Login/Login.jsx";
+import Register from "./Components/Register/Register.jsx";
 
-createRoot(document.getElementById('root')).render(
+const router = createBrowserRouter([
+  {
+    path: "/",
+    Component: Root,
+    children: [
+      {
+        index: true,
+        Component: Home,
+      },
+      {
+        path: "/login",
+        Component: Login,
+      },
+      {
+        path: "/register",
+        Component: Register,
+      },
+    ],
+  },
+]);
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <RouterProvider router={router}></RouterProvider>
+  </StrictMode>
+);
